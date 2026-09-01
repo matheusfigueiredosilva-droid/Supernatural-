bf9c639129b89f5564644a2e52465a5624949ed9
matheusfigueiredosilva-droid <matheus.figueiredo.silva@escola.pr.gov.br>
2026-08-31 22:44:05 -0300

    Add curiosity button functionality to script.js

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ let botao = document.querySelector("#botao-like");
 botao.addEventListener("click", function () {
     botao.textContent = "♥ Curtido!";
 });
+let botao = document.querySelector("#botao-like");
+
+botao.addEventListener("click", function () {
+    botao.textContent = "♥ Curtido!";
+});
+
+
+let botaoCuriosidade = document.querySelector("#botao-curiosidade");
+
+let curiosidade = document.querySelector("#curiosidade");
+
+botaoCuriosidade.addEventListener("click", function () {
+    curiosidade.textContent = "Curiosidade: o Chevrolet Impala 1967 usado por Dean se tornou um dos maiores símbolos de Supernatural e é carinhosamente chamado pelos fãs de Baby!";
+});
